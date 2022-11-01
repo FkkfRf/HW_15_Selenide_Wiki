db15651d39f8a61267ff5a090135cca459a9e56e
FkkfRf <Fkkf.Rf@gmail.com>
2022-11-01 22:21:41 +0300

    All tests

---
+++ src/test/java/otherTests/ContributerTest.java
@@ -0,0 +1,20 @@
+package otherTests;
+
+import org.junit.jupiter.api.Test;
+import selenideTests.BaseTest;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class ContributerTest extends BaseTest {
+    @Test
+    void searchContributer() {
+        open("/selenide/selenide");
+        $(".Layout-sidebar").$(byText("Contributors"))
+                .ancestor(".BorderGrid-cell").$$("ul li")
+                .first().hover();
+        $$(".Popover-message").findBy(visible).shouldHave(text("Alexei Vinogradov"));
+
+    }
+}
--- src/test/java/selenideTests/SelenideWikiTest.java
+++ src/test/java/selenideTests/SelenideWikiTest.java
@@ -2,16 +2,24 @@ package selenideTests;
 
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.partialText;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideWikiTest extends BaseTest{
-
     @Test
     void searchSelenideInGitHub(){
         open(baseUrl);
         $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
         $$(".repo-list li").first().$("a").click();
+        $("#repository-container-header").shouldHave(text("selenide\n/\nselenide\n"));
+        $("#wiki-tab").click();
+        $(".wiki-more-pages-link button").click();
+        $(".wiki-rightbar").shouldHave(text("SoftAssertions"));
+        $(".wiki-rightbar").$(byText("SoftAssertions")).click();
+        $(".markdown-body").shouldHave(partialText("Using JUnit5"));
     }
 
 }
